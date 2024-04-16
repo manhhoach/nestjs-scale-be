c7c8752c2e13c0e353a7981409b69952962e60ba
Mạnh Hoạch <manhhn.nv5@gmail.com>
2024-04-16 13:20:58 +0700

    .

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -7,6 +7,8 @@ import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'Joi';
 
 @Module({
   imports: [
@@ -15,6 +17,13 @@ import {
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@app/common';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from '@nestjs/config';
-import { LoggerModule } from '@app/common';
 
 @Module({
   imports: [
-   // LoggerModule,
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGO_URI'),
       }),
